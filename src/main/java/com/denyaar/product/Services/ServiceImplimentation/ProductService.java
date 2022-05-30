0a361da1e05b2bbdbeb2bed0148a79989769cc8a
Denyaar <nmupezeni@gmail.com>
2022-05-30 10:01:23 +0200

    Ammended Controller and service

--- src/main/java/com/denyaar/product/Services/ServiceImplimentation/ProductService.java
+++ src/main/java/com/denyaar/product/Services/ServiceImplimentation/ProductService.java
@@ -5,35 +5,30 @@ import com.denyaar.product.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class ProductService {
 
     @Autowired
     private ProductRepository productRepository;
 
-    public Product saveProduct(Product product){
+    public void  saveProduct(Product product){
         productRepository.save(product);
-        return product;
     }
 
     public List<Product> allProducts(){
         return (List<Product>) productRepository.findAll();
     }
 
-
-    public Product getProductById(Long id){
-        Optional<Product> optional=productRepository.findById(id);
-        Product product = null;
-        if(optional.isPresent()){
-            product=optional.get();
-        }else {
-            throw new RuntimeException("Product not available" + id);
-        }
-        return product;
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).get();
     }
+
+
     public void deleteProductById(long id){
         this.productRepository.deleteById(id);
     }
--- src/main/java/com/denyaar/product/controller/ProductController.java
+++ src/main/java/com/denyaar/product/controller/ProductController.java
@@ -9,39 +9,51 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
-@RequestMapping("/api")
-
 public class ProductController {
     @Autowired
     private ProductService productService;
 
 
-    @GetMapping("/products/")
-    List<Product> all() {
+    @GetMapping("/productList")
+    public List<Product> list() {
         return productService.allProducts();
     }
-    
-    
-    @PostMapping("/addProduct")
-    public ResponseEntity<Product> createTutorial(@RequestBody Product product) {
+
+    @GetMapping("/getProduct/{id}")
+    public ResponseEntity<Product> get(@PathVariable Long id) {
         try {
+            Product product = productService.getProductById(id);
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @PostMapping("/productSave")
+    public void add(@RequestBody Product product) {
+        productService.saveProduct(product);
+    }
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@RequestBody Product product, @PathVariable Long id) {
+        try {
+            Product existProduct = productService.getProductById(id);
+            product.setId(id);
             productService.saveProduct(product);
-            return new ResponseEntity<>(product, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
-
-    @DeleteMapping("/employees/{id}")
-    void deleteEmployee(@PathVariable Long id) {
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable Integer id) {
         productService.deleteProductById(id);
     }
-    
-    
-    
+
+
+
 }
 
 
